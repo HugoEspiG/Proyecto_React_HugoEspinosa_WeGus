83e8b75f2118b4bac27e3f73ec6ed7c0a3c3810b
HugoEspiG <hugo.espinosa01@correo.usa.edu.co>
2022-08-18 01:36:00 -0500

    (Prueba de uso git en usb)

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -11,7 +11,7 @@ import "./NavBar.css";
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <a class="navbar-brand" href="#">WeGus</a>
+        <NavLink to="/" class="navbar-brand">WeGus</NavLink>
         <CartWidget></CartWidget>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
